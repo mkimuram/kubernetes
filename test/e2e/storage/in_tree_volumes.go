57541688252c3a93e4be4dbaf4a1a556e5bcb33d
Masaki Kimura <masaki.kimura@hitachivantara.com>
2019-03-06 21:01:14 +0000

    Move disruptive tests to testsuites and add ones for block volume

--- test/e2e/storage/in_tree_volumes.go
+++ test/e2e/storage/in_tree_volumes.go
@@ -56,6 +56,7 @@ var testSuites = []func() testsuites.TestSuite{
 	testsuites.InitSubPathTestSuite,
 	testsuites.InitProvisioningTestSuite,
 	testsuites.InitMultiVolumeTestSuite,
+	testsuites.InitDisruptiveTestSuite,
 }
 
 // This executes testSuites for in-tree volumes.
